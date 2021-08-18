8b4dffe221ed8966708df78967355b9755968a76
Deepak Goyal <dg887180@gmail.com>
2021-08-18 20:08:06 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const luckyNo=document.querySelector("#lucky-no");
+const checkBtn=document.querySelector("#check-btn");
+const birthDay=document.querySelector("#birthday");
+const outputMsg=document.querySelector('#output-msg')
+
+checkBtn.addEventListener('click', showData);
+
+
+
+function showData(){
+    const birthDate=birthDay.value;
+    const luckyNumber=luckyNo.value        
+        const sum=calculateSum(birthDate);
+        if(sum&&birthDate){
+            compareValues(sum,luckyNumber)
+        }else{
+            outputMsg.innerText="Please fill both the input fields"
+        }
+    
+}
+
+function compareValues(sum,luckyNumber){
+    if(sum%luckyNumber===0){
+        outputMsg.innerText="🙂Yay,You are lucky";
+    }else{
+        outputMsg.innerText="☹️Sorry, You are not lucky";
+    }
+}
+
+function calculateSum(birthDate){
+    birthDate=birthDate.replaceAll("-","");
+    let sum=0;
+    for(i=0;i<birthDate.length;i++){
+        sum=sum+Number(birthDate.charAt(i))
+    }
+    return sum ;
+}
